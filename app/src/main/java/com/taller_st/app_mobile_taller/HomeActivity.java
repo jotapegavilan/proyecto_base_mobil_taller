66ef695e8d95fedfdf48464f4b41f268fd0ed8ac
= <j.p.daniel.gavilan@gmail.com>
2022-10-20 18:34:12 -0300

    limpiando app

--- app/src/main/java/com/taller_st/app_mobile_taller/HomeActivity.java
+++ app/src/main/java/com/taller_st/app_mobile_taller/HomeActivity.java
@@ -1,37 +1,13 @@
 package com.taller_st.app_mobile_taller;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
-import com.taller_st.app_mobile_taller.adaptadores.ProjectAdapter;
-import com.taller_st.app_mobile_taller.modelos.Project;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     private FloatingActionButton fbtnAgregar;
